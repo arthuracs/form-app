931dea53d8053e3a831a7fe4ec372b4d7c6f931c
Arthur Suermondt <arthuracs@gmail.com>
2022-03-22 15:04:07 +0100

    Convert Form component to functional component

--- src/components/Form.js
+++ src/components/Form.js
@@ -43,121 +43,99 @@ function Results({response, onReset}) {
     )
 }
 
-export default class Form extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.initialFormState = {
-            fullName: '',
-            email: '',
-            phone: '',
-            country: '',
-            petPref: '',
-            willLearn: false
-        }
-
-        this.state = {
-            form: this.initialFormState,
-            submitted: false
-        }
-
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.handleReset = this.handleReset.bind(this)
-        
-        this.countryArr = getSortedCountryList();
-
+export default function Form() {
+    const initialFormState = {
+        fullName: '',
+        email: '',
+        phone: '',
+        country: '',
+        petPref: '',
+        willLearn: false
     }
-    handleChange(event) {
+    const [form, setForm] = React.useState(initialFormState);
+    const [submitted, setSubmitted] = React.useState(false);
+    const countryArr = getSortedCountryList();
+    const handleChange = (event) => {
         const target = event.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
 
-        this.setState(({form}) => ({
-            form: {
-                ...form,
-                [name]: value
-            }
-        }))
+        setForm({
+            ...form,
+            [name]: value
+        })
     }
-    handleSubmit(event) {
+    const handleSubmit = (event) => {
         event.preventDefault();
 
-        this.setState({
-            submitted: true
-        })
+        setSubmitted(true);
     }
-    handleReset() {
-        this.setState({
-            form: this.initialFormState,
-            submitted: false
-        })
+    const handleReset = () => {
+        setForm(initialFormState);
+        setSubmitted(false);
     }
-    render() {
-
-        if (this.state.submitted === true) {
-            return <Results response={this.state.form} onReset={() => this.handleReset()} />
-        }
-
-        const inputFields = {
-            fullName: "Full name",
-            email: "E-mail",
-            phone: "Phone"
-        };
-        
-        return (
-            <form className="form" onSubmit={this.handleSubmit}>
-                
-                { Object.entries(inputFields).map(([id, label]) => (
-                    <React.Fragment key={id}>
-                        <Label forId={id}>
-                            {label}
-                        </Label>
-                        <Input 
-                            id={id}
-                            inputValue={this.state.form[id]} 
-                            onChange={this.handleChange} 
-                        />
-                    </React.Fragment>
-                ))
-                }
-
-                <Label forId="country">
-                    Country
-                </Label>
-                <Select 
-                    id="country" 
-                    inputValue={this.state.form.country} 
-                    onChange={this.handleChange} 
-                    options={this.countryArr} 
-                    placeholder="Select a country..."
-                />
-
-                <Radio
-                    id="petPref"
-                    inputValue={this.state.form.petPref}
-                    onChange={this.handleChange}
-                    options={["Cats", "Dogs"]}
-                >
-                    Cats or dogs?
-                </Radio>
-
-                <Checkbox 
-                    id="willLearn"
-                    checked={this.state.form.willLearn}
-                    onChange={this.handleChange}
-                >
-                    I promise I'll keep learning ;)
-                </Checkbox>
-
-                <Button 
-                    label="Submit"
-                    click={this.onSubmit}
-                    type="primary" 
-                    disabled={Object.values(this.state.form).every(x => x === '' || x === null || x === false)} 
-                />
-
-            </form>
-        )
+    
+    if (submitted === true) {
+        return <Results response={form} onReset={() => handleReset()} />
     }
+
+    const inputFields = {
+        fullName: "Full name",
+        email: "E-mail",
+        phone: "Phone"
+    };
+    return (
+        <form className="form" onSubmit={handleSubmit}>
+            
+            { Object.entries(inputFields).map(([id, label]) => (
+                <React.Fragment key={id}>
+                    <Label forId={id}>
+                        {label}
+                    </Label>
+                    <Input 
+                        id={id}
+                        inputValue={form[id]} 
+                        onChange={handleChange} 
+                    />
+                </React.Fragment>
+            ))
+            }
+
+            <Label forId="country">
+                Country
+            </Label>
+            <Select 
+                id="country" 
+                inputValue={form.country} 
+                onChange={handleChange} 
+                options={countryArr} 
+                placeholder="Select a country..."
+            />
+
+            <Radio
+                id="petPref"
+                inputValue={form.petPref}
+                onChange={handleChange}
+                options={["Cats", "Dogs"]}
+            >
+                Cats or dogs?
+            </Radio>
+
+            <Checkbox 
+                id="willLearn"
+                checked={form.willLearn}
+                onChange={handleChange}
+            >
+                I promise I'll keep learning ;)
+            </Checkbox>
+
+            <Button 
+                label="Submit"
+                click={handleSubmit}
+                type="primary" 
+                disabled={Object.values(form).every(x => x === '' || x === null || x === false)} 
+            />
+
+        </form>
+    )
 }
